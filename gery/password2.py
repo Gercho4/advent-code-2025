cc379c61717e7c9d1b218a168e9ac763456ddbda
Gergana Konakova <gergana.konakova@lobelia.earth>
2025-12-02 16:00:59 +0100

    Second part

---
+++ gery/password2.py
@@ -0,0 +1,82 @@
+file = open("gery/list")
+initlist = []
+
+for i in file:
+    initlist.append(i.strip("\n"))
+# print(initlist)
+state = 0
+lst2 = []
+
+
+#
+def right(state, directions, lst2):
+    res = state + directions
+    if res > 99 < 999:
+        forthelist = str(abs(res))
+        # print(forthelist)
+        lst2.append(forthelist[0])
+        # print(lst2)
+        res = res % 100
+        return (res, lst2)
+    elif res > 999:
+        forthelist = str(abs(res))
+        # print(forthelist)
+        lst2.append(forthelist[0])
+        # print(lst2)
+        res = res % 1000
+        return (res, lst2)
+    else:
+        # print(res)
+        # print(lst2)
+        return res, lst2
+
+
+def left(state, directions, lst2):
+    res = state - directions
+    if res < 0 > -999:
+        forthelist = str(abs(res))
+        # print(forthelist)
+        lst2.append(forthelist[0])
+        # print(lst2)
+        res = abs(res % 100)
+        return (res, lst2)
+    elif res < -999:
+        forthelist = str(abs(res))
+        # print(forthelist)
+        lst2.append(forthelist[0])
+        # print(lst2)
+        res = abs(res % 1000)
+        return (res, lst2)
+    else:
+        # print(res)
+        # print(lst2)
+        return res, lst2
+
+
+password = []
+result = 0
+init = 50
+for y in initlist:
+    if y[0] == "R":
+        dir = int(y[1:])
+        result, lst2 = right(init, dir, lst2)
+        init = result
+        # print(init)
+        if result == 0:
+            password.append(result)
+        # print(init)
+    if y[0] == "L":
+        dir = int(y[1:])
+        result, lst2 = left(init, dir, lst2)
+        init = result
+        # print(init)
+        if result == 0:
+            password.append(result)
+        # print(password)
+
+int_list = [int(x) for x in lst2]
+
+total = len(password) + sum(int_list)
+print(total)
+if __name__ == "__main__":
+    print(len(password))
